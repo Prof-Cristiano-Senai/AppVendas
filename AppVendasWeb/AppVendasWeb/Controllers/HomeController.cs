8464ad8dad443c376f9c0b0e91040d49eecad3a4
Prof. Cristiano de Paula <cristiano_paula@docente.senai.br>
2024-02-16 11:23:45 -0300

    Aula Manhã

--- AppVendasWeb/AppVendasWeb/Controllers/HomeController.cs
+++ AppVendasWeb/AppVendasWeb/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using AppVendasWeb.Data;
 using AppVendasWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json.Linq;
 using System.Diagnostics;
 
 namespace AppVendasWeb.Controllers
@@ -29,6 +30,7 @@ namespace AppVendasWeb.Controllers
             ViewData["ListaClientes"] = listaClientes;
             ViewData["ListaProdutos"] = listaProdutos;
             ViewData["ClienteSelecionado"] = "Nenhum cliente selecionado";
+            ViewData["IdSelecionado"] = "Nenhum cliente selecionado";
             return View();
         }
 
@@ -42,6 +44,7 @@ namespace AppVendasWeb.Controllers
             if (cliente != null)
             {
                 ViewData["ClienteSelecionado"] = cliente.ClienteNome;
+                ViewData["IdSelecionado"] = cliente.ClienteId;
             }
             return View("IniciarVenda");
         }
@@ -60,11 +63,17 @@ namespace AppVendasWeb.Controllers
             return View("IniciarVenda");
         }
 
-        public IActionResult SalvarVenda()
+        [HttpPost]
+        public async Task<IActionResult> IniciarVenda(
+            [Bind("NovaVendaId,DataVenda,ValorTotal,NotaFiscal,ClienteId,TotalProdutos, TotalDesconto, PercentualDesconto,TotalFinal")]
+                NovaVenda novaVenda)
         {
-            // Lógica para salvar a venda
+            List<Cliente> listaClientes = _context.Cliente.Where(c => c.CadastroAtivo == true).ToList();
+            List<Produto> listaProdutos = _context.Produtos.OrderBy(p => p.Descricao).ToList();
+            ViewData["ListaClientes"] = listaClientes;
+            ViewData["ListaProdutos"] = listaProdutos;
 
-            return View("IniciarVenda");
+            return View("IniciarVenda", novaVenda);
         }
 
         public IActionResult Privacy()
